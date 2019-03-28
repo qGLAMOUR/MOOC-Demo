9a99a5b0a83a9f4990c635e413e6457cc0b67fea
Glamour <1733782645@qq.com>
2019-03-28 11:19:11 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+项目运行入口
+'''
+
+import time
+import requests
+import ssl
+from config import *
+
+
+def get_index_html():
+    url = "https://www.icourse163.org/"
+    # ssl._create_default_https_context = ssl._create_unverified_context
+    r = requests.get(url)
+    print(r.text)
+
+
+def main():
+    get_index_html()
+
+
+if __name__ == '__main__':
+    main()
